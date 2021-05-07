6525f5f2fdb93e75520b13961d3d9bb95a9af25a
SteeOU <stee.ou@gmail.com>
2021-05-07 18:06:44 +0300

    Preparing to create a function

--- lesson_13/01_ticket.py
+++ lesson_13/01_ticket.py
@@ -3,8 +3,13 @@
 # и заполняющую ими шаблон билета Airline.
 # Подходящий шрифт искать на сайте
 
+from PIL import Image, ImageFont, ImageDraw
 
 
+image = Image.open('ticket_template.png')
+font = ImageFont.truetype('Ubuntu.ttf', 20)
 
-
+draw = ImageDraw.Draw(image)
+draw.text((100, 100), 'Hello', font=font)
+image.show()
 
