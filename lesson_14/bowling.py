78d22a7060b714eac12a3f2993bda09b0e2873a0
SteeOU <stee.ou@gmail.com>
2021-05-25 15:43:11 +0300

    Try to make pattern 'state'

--- lesson_14/bowling.py
+++ lesson_14/bowling.py
@@ -1,3 +1,34 @@
+from abc import ABCMeta, abstractmethod
+
+
+class State(metaclass=ABCMeta):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    @abstractmethod
+    def take_score(self):
+        pass
+
+
+class SymbolState(State):
+
+    def take_score(self):
+        pass
+
+
+class DigitalState(State):
+    def take_score(self):
+        pass
+
+
+class BowlingScore:
+    def __init__(self):
+        self.symbol_state = SymbolState(self)
+        self.digital_state = DigitalState(self)
+        self.total_score = 0
+        self.buffer_symbol = 0
+
+
 def get_score(game_result):
     score = 0
     score += game_result.count('X') * 20
