16076118d20be93f65946623b41266c75913f2fc
SteeOU <stee.ou@gmail.com>
2021-05-26 16:37:25 +0300

    Pattern 'state', correct algorithm (correct)

--- lesson_14/bowling.py
+++ lesson_14/bowling.py
@@ -13,6 +13,9 @@ class State(metaclass=ABCMeta):
         pass
 
 
+# res = "X4/3--44/-"
+
+
 class SymbolState(State):
     def take_score(self, char):
         if char == 'X':
@@ -20,8 +23,11 @@ class SymbolState(State):
         elif char.isdigit():
             self.object_get_score.buffer_symbol = char
             self.object_get_score._state = self.object_get_score.digital_state
-        elif self.object_get_score.buffer_symbol != '-':
+        elif self.object_get_score.buffer_symbol == '-':
             self.object_get_score.buffer_symbol = char
+        elif char == '/' and self.object_get_score.buffer_symbol.isdigit():
+            self.object_get_score.total_score += SPARE
+            self.object_get_score.total_score -= int(self.object_get_score.buffer_symbol)
 
 
 class DigitalState(State):
@@ -38,6 +44,10 @@ class DigitalState(State):
             else:
                 self.object_get_score.total_score += int(self.object_get_score.buffer_symbol)
                 self.object_get_score.buffer_symbol = char
+                self.object_get_score._state = self.object_get_score.symbol_state
+                if char.isdigit():
+                    self.object_get_score.total_score += int(char)
+
 
 
 class BowlingScore:
@@ -53,27 +63,32 @@ class BowlingScore:
             self._state.take_score(char)
 
 
-# def get_score(game_result):
-#     score = 0
-#     score += game_result.count('X') * 20
-#     buffer = game_result.replace("X", '')
-#     while '/' in buffer:
-#         index = buffer.find('/')
-#         buffer = buffer[:index - 1] + buffer[index + 1:]
-#         score += 15
-#     for i in list(buffer):
-#         if i == '-':
-#             continue
-#         else:
-#             score += int(i)
-#     return score
+def get_score(game_result):
+    score = 0
+    score += game_result.count('X') * 20
+    buffer = game_result.replace("X", '')
+    while '/' in buffer:
+        index = buffer.find('/')
+        buffer = buffer[:index - 1] + buffer[index + 1:]
+        score += 15
+    for i in list(buffer):
+        if i == '-':
+            continue
+        else:
+            score += int(i)
+    return score
 
-#     "X4/34-4",
-#     "123/XXXXXX--X"
-res = "X4/3---5/-"
-# res = "--X154/"
+#     "X4/3--45/--",
+#     "X4/3--45/--"
+res = "285-7/4/3/277-2---"
+#"6---15--142/336/24X"
+# "3/15--8117--3628X1/"
+# "8-X3-4-41-10----7-X"
+# "285-7/4/3/277-2---"
 # print(get_score(res))
 
 result = BowlingScore()
 result.score(res)
 print(result.total_score)
+print(get_score(res))
+
