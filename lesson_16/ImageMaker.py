b382980735a9e8cd83c1f0d3c4a71aae595820a7
SteeOU <stee.ou@gmail.com>
2021-06-22 16:00:41 +0300

    Сделана функция по созданию макетов для прогноза и проверка, имеются ли они в текущем каталоге (для повторного их использования) Доделать: проверку всех файлов-макетов и т.д.

--- lesson_16/ImageMaker.py
+++ lesson_16/ImageMaker.py
@@ -4,14 +4,28 @@
 #   С текстом, состоящим из полученных данных (пригодится cv2.putText)
 #   С изображением, соответствующим типу погоды
 #   В качестве фона добавить градиент цвета, отражающего тип погоды
-# Солнечно - от желтого к белому
-# Дождь - от синего к белому
-# Снег - от голубого к белому
-# Облачно - от серого к белому
+# Солнечно, ясно, малооблачно, облачно с прояснениями - от желтого к белому
+# Дождь, небольшой дождь, град, ливень, гроза - от синего к белому
+# Снег, мокрый снег, снег с дождем, снегопад , обледенение, метель- от голубого к белому
+# Облачно, пасмурно, туман - от серого к белому
+
+# {'21.06.2021': {'Погода': 'Облачно с прояснениями',# 'Температура днем': '+31', 'Температура ночью': '+18', 'Дата': '21.06.2021'},
+# '22.06.2021': {'Погода': 'Ясно', 'Температура днем': '+33', 'Температура ночью': '+20', 'Дата': '22.06.2021'},
+# '23.06.2021': {'Погода': 'Малооблачно', 'Температура днем': '+34', 'Температура ночью': '+21', 'Дата': '23.06.2021'},
+# '24.06.2021': {'Погода': 'Малооблачно', 'Температура днем': '+33', 'Температура ночью': '+21', 'Дата': '24.06.2021'},
+# '25.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+33', 'Температура ночью': '+19', 'Дата': '25.06.2021'},
+# '26.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+28', 'Температура ночью': '+19', 'Дата': '26.06.2021'},
+# '27.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+26', 'Температура ночью': '+18', 'Дата': '27.06.2021'},
+# '28.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+25', 'Температура ночью': '+17', 'Дата': '28.06.2021'},
+# '29.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+24', 'Температура ночью': '+15', 'Дата': '29.06.2021'},
+# '30.06.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+23', 'Температура ночью': '+15', 'Дата': '30.06.2021'},
+# '01.07.2021': {'Погода': 'Облачно с прояснениями', 'Температура днем': '+26', 'Температура ночью': '+16', 'Дата': '01.07.2021'},
+
+
 
 import cv2
 from datetime import datetime
-
+import os
 
 class ImageMaker:
     def __init__(self, date=datetime.today().strftime('%d.%m.%Y')):
@@ -19,36 +33,67 @@ class ImageMaker:
         self.blank = 'image/probe.jpg'
         self.b_color, self.g_color, self.r_color = 0, 0, 0
         self.b_color_delta, self.g_color_delta, self.r_color_delta = 0, 0, 0
+        self.font = cv2.FONT_HERSHEY_COMPLEX
+        self.color = cv2.COLOR_BAYER_BG2RGB
+        self.mockup_dict = {}
 
-    def draw_postcard(self):
+    def make_mockup(self):
         image = cv2.imread(self.blank)
         height, width = image.shape[0], image.shape[1]
-        foo = 4
-        if foo == 1:
-            self.b_color, self.g_color, self.r_color = 0, 255, 255
-            self.b_color_delta, self.g_color_delta, self.r_color_delta = round(255/height, 2), 0, 0
-        elif foo == 2:
-            self.b_color, self.g_color, self.r_color = 255, 0, 0
-            self.b_color_delta, self.g_color_delta, self.r_color_delta = 0, round(255/height, 2), round(255/height, 2)
-        elif foo == 3:
-            self.b_color, self.g_color, self.r_color = 255, 170, 85
-            self.b_color_delta, self.g_color_delta, self.r_color_delta = 0, round((255 - 170) / height, 2), round((255 - 85) / height, 2),
-        else:
-            self.b_color, self.g_color, self.r_color = 120, 120, 120
-            self.b_color_delta, self.g_color_delta, self.r_color_delta = round((255 - 120) / height, 2), round((255 - 120) / height, 2), round((255 - 120) / height, 2)
-        for x in range(0, width):
-            if foo == 3:
+        icon = cv2.imread('image/sun.png')
+        for mockup_number in range(4):
+            if mockup_number == 0:
+                self.b_color, self.g_color, self.r_color = 0, 255, 255
+                self.b_color_delta, self.g_color_delta, self.r_color_delta = round(255/height, 2), 0, 0
+                self.mockup_dict[mockup_number] = 'Sun.jpg'
+            elif mockup_number == 1:
+                self.b_color, self.g_color, self.r_color = 255, 0, 0
+                self.b_color_delta, self.g_color_delta, self.r_color_delta = 0, round(255/height, 2), round(255/height, 2)
+                self.mockup_dict[mockup_number] = 'Rain.jpg'
+                icon = cv2.imread('image/rain.png')
+            elif mockup_number == 2:
                 self.b_color, self.g_color, self.r_color = 255, 170, 85
-            elif foo == 4:
+                self.b_color_delta, self.g_color_delta, self.r_color_delta = 0, round((255 - 170) / height, 2), round((255 - 85) / height, 2),
+                self.mockup_dict[mockup_number] = 'Snow.jpg'
+                icon = cv2.imread('image/snow.png')
+            elif mockup_number == 3:
                 self.b_color, self.g_color, self.r_color = 120, 120, 120
-            for y in range(0, height):
-                image[y, x] = [self.b_color, self.g_color, self.r_color]
-                self.b_color += self.b_color_delta
-                self.g_color += self.g_color_delta
-                self.r_color += self.r_color_delta
+                self.b_color_delta, self.g_color_delta, self.r_color_delta = round((255 - 120) / height, 2), round((255 - 120) / height, 2), round((255 - 120) / height, 2)
+                self.mockup_dict[mockup_number] = 'Cloud.jpg'
+                icon = cv2.imread('image/cloud.png')
+            for x in range(0, width):
+                if mockup_number == 2:
+                    self.b_color, self.g_color, self.r_color = 255, 170, 85
+                elif mockup_number == 3:
+                    self.b_color, self.g_color, self.r_color = 120, 120, 120
+                for y in range(0, height):
+                    image[y, x] = [self.b_color, self.g_color, self.r_color]
+                    self.b_color += self.b_color_delta
+                    self.g_color += self.g_color_delta
+                    self.r_color += self.r_color_delta
+            roi = image[0:icon.shape[0], int(width - icon.shape[1]):width]
+            icon_gray = cv2.cvtColor(icon, cv2.COLOR_BGR2GRAY)
+            ret, mask = cv2.threshold(icon_gray, 10, 255, cv2.THRESH_BINARY)
+            mask_inv = cv2.bitwise_not(mask)
+            img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+            icon_fg = cv2.bitwise_and(icon, icon, mask=mask)
+            dst = cv2.add(img_bg, icon_fg)
+            image[0:icon.shape[0], int(width - icon.shape[1]):width] = dst
+            cv2.imwrite(self.mockup_dict[mockup_number], image)
 
+    def draw_postcard(self):
+        check_img = os.path.exists('Cloud.jpg')
+        if not check_img:
+            print('KEK')
+            self.make_mockup()
+        image = cv2.imread('Rain.jpg')
+        cv2.putText(image, self.date, (210, 30), self.font, 1, self.color)
+        cv2.putText(image, 'Погода', (50, 30), self.font, 1, self.color)
+        cv2.putText(image, f'Температура днем: {self.r_color_delta}', (5, 90), self.font, 1, self.color)
+        cv2.putText(image, f'Температура ночью: {self.r_color_delta}', (5, 140), self.font, 1, self.color)
+        cv2.putText(image, f'{self.r_color_delta}', (5, 190), self.font, 1, self.color)
+        cv2.putText(image, 'У природы нет плохой погоды!', (80, 245), self.font, 0.6, self.color)
 
-        cv2.putText(image, self.date, (80, 80), cv2.FONT_HERSHEY_COMPLEX, 2, cv2.COLOR_BAYER_BG2RGB)
         cv2.imshow('Test', image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- lesson_16/WeatherMaker.py
+++ lesson_16/WeatherMaker.py
@@ -18,16 +18,23 @@ class WeatherMaker:
         site = requests.get('https://yandex.by/pogoda/mogilev')
         html_doc = BeautifulSoup(site.text, features='html.parser')
         list_day = html_doc.find_all('time', {'class': 'time forecast-briefly__date'})
-        temperature_days = html_doc.find_all('div', {"class": "temp forecast-briefly__temp forecast-briefly__temp_day"})
-        temperature_nights = html_doc.find_all('div',
-                                              {"class": "temp forecast-briefly__temp forecast-briefly__temp_night"})
+        temperature_days = html_doc.find_all(
+            'div',
+            {"class": "temp forecast-briefly__temp forecast-briefly__temp_day"}
+        )
+        temperature_nights = html_doc.find_all(
+            'div',
+            {"class": "temp forecast-briefly__temp forecast-briefly__temp_night"}
+        )
         state_weather = html_doc.find_all('div', {'class': "forecast-briefly__condition"})
         for date, t_day, t_night, state in zip(list_day, temperature_days, temperature_nights, state_weather):
             correct_date = datetime.strptime(date.get('datetime')[:10], '%Y-%m-%d').strftime('%d.%m.%Y')
-            self.weather_dict[correct_date] = {'Погода': state.text,
-                                                    'Температура днем': re.search(self.re_temperature, t_day.text).group(),
-                                                    'Температура ночью': re.search(self.re_temperature, t_night.text).group(),
-                                                    'Дата': correct_date}
+            self.weather_dict[correct_date] = {
+                'Погода': state.text,
+                'Температура днем': re.search(self.re_temperature, t_day.text).group(),
+                'Температура ночью': re.search(self.re_temperature, t_night.text).group(),
+                'Дата': correct_date
+            }
         return self.weather_dict
 
 
