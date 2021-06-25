7f1b67f0b3c8e0088b665079f0a3addb7ab5da4a
SteeOU <stee.ou@gmail.com>
2021-06-25 18:32:03 +0300

    База данных - начало

---
+++ lesson_16/databaseUpdater.py
@@ -0,0 +1,14 @@
+# Добавить класс DatabaseUpdater с методами:
+#   Получающим данные из базы данных за указанный диапазон дат.
+#   Сохраняющим прогнозы в базу данных (использовать peewee)
+
+
+class DatabaseUpdater:
+    def get_data(self):
+        pass
+
+    def save_data(self):
+        pass
+
+
+
---
+++ lesson_16/models.py
@@ -0,0 +1,20 @@
+# Рекомендации:
+# Можно создать отдельный модуль для инициализирования базы данных.
+# Как далее использовать эту базу данных в движке:
+# Передавать DatabaseUpdater url-путь
+# https://peewee.readthedocs.io/en/latest/peewee/playhouse.html#db-url
+# Приконнектится по полученному url-пути к базе данных
+# Инициализировать её через DatabaseProxy()
+# https://peewee.readthedocs.io/en/latest/peewee/database.html#dynamically-defining-a-database
+
+import peewee
+
+database = peewee.SqliteDatabase('weather.bd')
+
+
+class DataWeather(peewee.Model):
+    pass
+
+    class Meta:
+        database = database
+
